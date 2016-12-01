9cb5bfc2d92781a9fca46e2a9bd68ef4bbcf90e0
Jason Maiorana <Jason.Maiorana@capitalone.com>
2016-12-01 13:26:14 -0500

    Park builders not building near spawn

--- swampy1/role.builder.js
+++ swampy1/role.builder.js
@@ -9,22 +9,37 @@ var roleBuilder = {
 
 	    if(creep.memory.building && creep.carry.energy === 0) {
             creep.memory.building = false;
-			util.set_doing_state(creep, "harvesting");
 	    }
-	    if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
+		else if(!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
 	        creep.memory.building = true;
-			util.set_doing_state(creep, "building");
 	    }
 
-	    if(creep.memory.building) {
+	    if(creep.memory.building)
+		{
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+            if(targets.length)
+			{
+				util.set_doing_state(creep, "building");
+                if(creep.build(targets[0]) === ERR_NOT_IN_RANGE)
+				{
                     creep.moveTo(targets[0]);
                 }
             }
+			else
+			{
+				util.set_doing_state(creep, "park");
+				targets = creep.room.find(FIND_STRUCTURES, 
+						{ filter: (structure) => { return (structure.structureType == STRUCTURE_SPAWN ); } });
+				if(targets.length > 0)
+				{
+					let dest_pos = targets[0].pos;
+					creep.moveTo(dest_pos.x, dest_pos.y - 3);
+				}
+			}
 	    }
-	    else {
+	    else
+		{
+			util.set_doing_state(creep, "harvesting");
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
