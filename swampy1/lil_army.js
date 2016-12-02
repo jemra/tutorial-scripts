14feeef9dc6e84164b3eebec3e71baf9d4bca00f
Jason Maiorana <Jason.Maiorana@capitalone.com>
2016-12-02 14:59:42 -0500

    Improve build order

--- swampy1/lil_army.js
+++ swampy1/lil_army.js
@@ -12,10 +12,12 @@ let _army_composition =
 [
     {"model":"baby_harvester", "count" : 2},
     {"model":"baby_builder", "count": 1},
-    {"model":"baby_upgrader", "count" : 1},
     {"model":"killbot", "count" : 5},
+    {"model":"baby_upgrader", "count" : 1},
     {"model":"baby_harvester", "count" : 6},
     {"model":"baby_builder", "count": 3},
+    {"model":"baby_upgrader", "count" : 2},
+    {"model":"killbot", "count" : 9},
     {"model":"baby_upgrader", "count" : 3},
 ];
 
