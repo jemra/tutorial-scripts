314eb5da7488dc295ef6f956b2f276a29bdbe7e7
Jason Maiorana <Jason.Maiorana@capitalone.com>
2016-12-08 11:25:47 -0500

    prioritize tower uber alles

--- swampy1/mining_planner.js
+++ swampy1/mining_planner.js
@@ -119,10 +119,25 @@ function refresh_room_mining_plan(room_name)
 			}
 		});
 	if(retrieve_targets.length)
-		mine_map.retrieve_to = retrieve_targets.map( a => a.id);
-	else
-		mine_map.retrieve_to = null;
+	{
+		retrieve_targets = retrieve_targets.sort( function(a,b)
+		{
+			if( a.structureType != b.structureType )
+			{
+				if( a.structureType === STRUCTURE_TOWER )
+					return -1;
+				else if( b.structureType === STRUCTURE_TOWER )
+					return 1;
+			}
+			return a.id - b.id;
+		});
 
+		mine_map.retrieve_to = retrieve_targets.map( a => a.id);
+	}
+	else
+	{
+		mine_map.retrieve_to = null;
+	}
 	// find building targets
 	let build_targets = room.find(FIND_CONSTRUCTION_SITES);
 	if(build_targets.length)
